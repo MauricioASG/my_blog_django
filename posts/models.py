e30b43f8fc4666b99c3393077582ee08d97d8d6c
Mauricio Alejandro Serrano García <mauricioalejandroserranogarcia@gmail.com>
2026-02-08 22:25:53 -0600

    Primera migracion de prueba

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# creamos una clase para el modelo
+class Post(models.Model):
+  title = models.CharField(max_length=255)
+  description = models.TextField()
+  created_at = models.DateField(auto_created=True)
+  
