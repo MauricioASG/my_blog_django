d5ee60fefb74c881ab193a1149c6704f3d6ecead
Mauricio Alejandro Serrano García <mauricioalejandroserranogarcia@gmail.com>
2026-02-08 19:12:14 -0600

    creando primera vista

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.http import HttpResponse
 
+class HelloWorld(View):
+  def get(self, request):
+    return HttpResponse(content='Hola mundo soy Mauricio')
 # Create your views here.
