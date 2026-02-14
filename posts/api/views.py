73f39ba88c5eae4cc4c4763998016ca03f693713
Mauricio Alejandro Serrano García <mauricioalejandroserranogarcia@gmail.com>
2026-02-14 11:48:08 -0600

    se adapta para regresar contenido desde la bd

--- posts/api/views.py
+++ posts/api/views.py
@@ -2,10 +2,17 @@ from rest_framework.views import APIView #Peticiones http
 from rest_framework.response import Response
 from rest_framework import status
 
+#Importamos el modelo
+from posts.models import Post
+
 
 class PostApiView(APIView):
   # Metodo para obtener los post
   def get(self, request):
+    # obtenemos todos los elementos del modelo Post y los guarde en la varibale "posts"
+    # posts = Post.objects.all() 
+    # objetemos todos los post, hacemos un bucle y por cada post vamos a decirle que guarde el array por cada post
+    posts = [post.title for post in Post.objects.all()] 
     # return necesita un response, resonde un status y un contenido data
-    return Response(status=status.HTTP_200_OK, data='Hola mundo')#
+    return Response(status=status.HTTP_200_OK, data=posts)#
   
